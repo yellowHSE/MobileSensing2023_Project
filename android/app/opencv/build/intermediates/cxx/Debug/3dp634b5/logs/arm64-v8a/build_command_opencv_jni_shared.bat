6d14d4701922581f5e5a67f7585ced98b5bc3cc6
yellowHSE <74814515+yellowHSE@users.noreply.github.com>
2024-01-21 22:52:12 +0100

    -

--- android/app/opencv/build/intermediates/cxx/Debug/3dp634b5/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\hse20\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\hse20\\Downloads\\opencv-4.8.0-android-sdk\\OpenCV-android-sdk\\sdk\\.cxx\\Debug\\3dp634b5\\arm64-v8a" ^
-  opencv_jni_shared
